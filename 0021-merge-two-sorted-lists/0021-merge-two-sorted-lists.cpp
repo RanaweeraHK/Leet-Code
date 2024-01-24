01e79ca87d2c0a340dc61005d7ac514995f759fd
Hashini Kaweesha Ranaweera <129282753+RanaweeraHK@users.noreply.github.com>
2024-01-24 09:41:35 +0530

    Time: 0 ms (100.00%), Space: 18.1 MB (5.36%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,46 +11,48 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* mergedList = nullptr;  //pointer to the head of the merged list
-        ListNode* current = nullptr;  //pointer to travel through the merged list
+        ListNode* mergedList = nullptr;
+        ListNode* current =nullptr;
         
         if(list1==nullptr){
             return list2;
         }
-        else if(list2 == nullptr){
+        else if(list2==nullptr){
             return list1;
         }
         else{
-            while(list1 != nullptr && list2 != nullptr){
-                if(list1->val <= list2->val){
+            while(list1!= nullptr && list2!= nullptr){
+                if(list1->val <list2->val){
                     if(mergedList == nullptr){
-                        mergedList=current = list1;
+                        mergedList=current =list1;
+                        
                     }
                     else{
-                        current->next =list1;
+                        current->next = list1;
                         current = current->next;
                     }
-                    list1=list1->next;
+                    list1= list1->next;
                 }
                 else{
                     if(mergedList == nullptr){
-                        mergedList=current = list2;
+                        mergedList=current =list2;
+                        
                     }
                     else{
-                        current->next =list2;
+                        current->next = list2;
                         current = current->next;
                     }
                     list2=list2->next;
                 }
             }
         }
+        
         if(list1!= nullptr){
-            current->next = list1;
+            current->next =list1;
         }
         else{
             current->next =list2;
         }
-        
         return mergedList;
     }
 };
